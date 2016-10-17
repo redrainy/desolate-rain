864e2bed0c06152eccd5ca1b431db6d8f2072ac8
redrainy <1564516977@qq.com>
2016-10-17 17:51:22 +0800

    login

---
+++ src/main/java/com/desolate/dao/LoginDaoImpl.java
@@ -0,0 +1,8 @@
+package com.desolate.dao;
+
+/**
+ * Created by Rainy on 2016/10/17/0017.
+ */
+public class LoginDaoImpl {
+
+}
---
+++ src/main/java/com/desolate/service/LoginServiceImpl.java
@@ -0,0 +1,8 @@
+package com.desolate.service;
+
+/**
+ * Created by Rainy on 2016/10/17/0017.
+ */
+public class LoginServiceImpl {
+
+}
